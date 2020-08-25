bcca043c1ad6533d9fb114c8b331322d2817fc40
Sania Abhari <sania.abhari@ibm.com>
2020-08-25 09:13:34 -0400

    reddit login

--- accessing_apis/reddit_api.py
+++ accessing_apis/reddit_api.py
@@ -5,21 +5,21 @@ from dotenv import load_dotenv
 import praw
 load_dotenv()
 
-reddit_key = os.getenv('reddit_api_key')
-reddit_username = os.getenv('reddit_username')
-reddit_pw = os.getenv('reddit_pw')
-reddit_app_name = os.getenv('reddit_app_name')
-reddit_personal_use_script = os.getenv('reddit_personal_use')
 
+reddit = praw.Reddit(client_id=os.getenv('reddit_personal_use'),
+                     client_secret=os.getenv('reddit_api_key'),
+                     user_agent=os.getenv('reddit_app_name'),
+                     username=os.getenv('reddit_username'),
+                     password=os.getenv('reddit_pw'))
 
-
-reddit = praw.Reddit(client_id=reddit_personal_use_script, \
-                     client_secret=reddit_key, \
-                     user_agent=reddit_app_name, \
-                     username=reddit_username, \
-                     password=reddit_pw)
-
+print(reddit.user.me())
 
 subreddit = reddit.subreddit('Coronavirus')
 top_subreddit = subreddit.top()
 top_subreddit = subreddit.top(limit=10)
+
+for submission in subreddit.top(limit=1):
+    print(submission.title, submission.id)
+
+for submission in top_subreddit:
+    topics_dict["title"].append(submission.title)

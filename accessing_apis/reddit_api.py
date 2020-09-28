eeebb6be13489427fa321bdd80a05a224c7e07a8
Sania Abhari <sania.abhari@ibm.com>
2020-09-28 12:08:38 -0400

    added pushshift draft, doesn't work yet.

--- accessing_apis/reddit_api.py
+++ accessing_apis/reddit_api.py
@@ -59,3 +59,59 @@ for index, submission in enumerate(subreddit):
     # Inserting a submission to the collection in the database
     # collection.insert_one(subreddit_output)
     posts.append(subreddit_output)
+
+# ------------------------------------------------- USING PUSHSHIFT -------------------------------------------------
+
+from psaw import PushshiftAPI
+
+# Uses pushshift search to fetch ids and then uses praw to fetch objects
+# 'reddit' was defined in line 11 using praw and the users personal api key and other information
+api = PushshiftAPI(reddit)
+
+
+start_epoch=int(dt.datetime(2020, 9, 23).timestamp())
+end_epoch=int(dt.datetime(2020, 9, 25).timestamp())
+result = api.search_submissions(after=start_epoch,
+                            subreddit='politics',
+                            filter=['url','author', 'title', 'subreddit'],
+                            limit=10)
+
+https://api.pushshift.io/reddit/search/comment/?q=trump&after=start_epochd&before=end_epochd&sort=asc
+https://api.pushshift.io/reddit/comment/search/?subreddit=politics&q=trump&after=start_epochd&before=end_epochd&sort=asc
+
+https://api.pushshift.io/reddit/comment/search/?subreddit=politics&q=trump&after=1600833600&before=1601006400&sort=asc
+https://api.pushshift.io/reddit/comment/search/?subreddit=politics&q=trump&after=1577854800&before=1601006400&sort=asc
+
+
+https://api.pushshift.io/reddit/search/comment/?q=trump&after=7d&aggs=subreddit&size=0
+
+api = PushshiftAPI()
+# The `search_comments` and `search_submissions` methods return generator objects
+gen = api.search_submissions(limit=100)
+next(gen)
+cache = []
+
+for c in gen:
+    print(c)
+    cache.append(c)
+
+results = list(gen)
+
+start_epoch=int(dt.datetime(2020, 1, 1).timestamp())
+
+list(api.search_submissions(after=start_epoch,
+                            subreddit='wallstreetbets',
+                            filter=['title','score', 'id', 'url','author','created_utc'],
+                            limit=10))
+
+
+import requests
+
+return requests.get(url).json()
+url = "https://api.pushshift.io/reddit/search/submission"
+params = {"subreddit": "ifyoulikeblank"}
+submissions = requests.get(url, params = params)
+type(submissions)
+last_submission_time = submissions.json()
+params = {"subreddit" : "ifyoulikeblank", "before" : last_submission_time}
+submissions = requests.get(url, params = params)
